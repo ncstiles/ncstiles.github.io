4bb9323fe70cb3e83c146f1751ad476fcf69093b
Nicole Stiles <ncstiles@fb.com>
2022-06-17 17:09:25 -0700

    Updated contents using other repos code

---
+++ README.md
@@ -0,0 +1,62 @@
+## Week 1 Assignment: Flixster
+
+Submitted by: Nicole Stiles
+
+Estimated time spent: 24hrs
+
+Deployed Application (optional): [Flixster Deployed Site](ADD_LINK_HERE)
+
+### Application Features
+
+#### CORE FEATURES
+
+- [x] User can view a list of current movies from The Movie Database API as a grid view
+  - [x] The grid element should have an id of `movies-grid`
+  - [x] Each movie wrapper element should have a class of `movie-card`
+- [x] For each movie displayed, user can see the following details:
+  - [x] Title - the element should have a class of `movie-title`
+  - [x] Image - the `img` element should have a class of `movie-poster`
+  - [x] Votes - the element should have a class of `movie-votes`
+- [x] User can load more current movies by clicking a button at the bottom of the list
+  - [x] The button should have an id of `load-more-movies-btn`.
+  - [x] When clicked, the page should not refresh.
+  - [x] New movies should simply be added to the bottom
+- [x] Allow users to search for movies and display them in a grid view
+  - [x] There should be a search input element with an id of `search-input`
+  - [x] Users should be able to type into the input
+  - [x] When a user hits 'Enter', it should send a search request to the movies API
+  - [x] The results from the search should be displayed on the page
+  - There should be a close icon with an id of `close-search-btn` that exits the search, clears results, and shows the current movies displayed previously
+- [x] Website accounts for basic HTML/CSS accessibility features
+- [x] Website should be responsive
+
+#### STRETCH FEATURES
+
+- [ ] Deploy website using GitHub Pages. 
+- [x] Allow user to view more details about a movie within an opaque card.
+- [x] Improve the user experience through CSS & animation (I implemented fade and opacity, and transition time).
+- [x] Allow movie video trailers to be played using [embedded YouTube](https://support.google.com/youtube/answer/171780?hl=en)
+- [ ] Implement anything else that you can get done to improve the app functionality!
+
+### Walkthrough Video
+
+![demo](https://user-images.githubusercontent.com/106721607/174413569-8602869b-9460-4d96-941b-933653b6b530.gif)
+
+### Reflection
+
+* Did the topics discussed in your labs prepare you to complete the assignment? Be specific, which features in your weekly assignment did you feel unprepared to complete?
+
+Yes!  The topics definitely prepared us.  The API lesson was very helpful.  There wasn't anything specific that I felt unprepared to do.  
+
+* If you had more time, what would you have done differently? Would you have added additional features? Changed the way your project responded to a particular event, etc.
+  
+If I had more time, I would spend more time making the website reactive, as well as work no the CSS alignment properties.
+
+* Reflect on your project demo, what went well? Were there things that maybe didn't go as planned? Did you notice something that your peer did that you would like to try next time?
+
+The API calls went well, but the css work didn't go as well.  I would like to work more on the transitions!  
+
+
+### Shout out
+
+Massive shoutout to Vincent for all the help through the project!
---
+++ script.js
@@ -0,0 +1,371 @@
+// import * as assert from 'assert'
+const apiKey = 'a47267c9b249b769d20e9c89247afa61';
+let moviesPerPage = 24;
+
+let numLoads = 0;
+let titles = [];
+let imgPaths = [];
+let ratings = [];
+let overviews = [];
+
+
+//TODO:remember after the search field is cleared to clear these arrays to save space
+let numSearchLoads = 0;
+let searchTitles = [];
+let searchImgPaths = [];
+let searchRatings = [];
+let searchOverviews = [];
+
+let textEntered = false;
+let submitted = false;
+
+
+/**
+ * 
+ * @returns an object literal containing the title, movie, rating and overview of recent movies
+ */
+async function getMovieData() {
+    const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`;
+
+    const results = await getData(url);
+    return results;
+}
+
+async function getSearchData(query) {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}`;
+
+    const results = await getData(url);
+    return results;
+
+}
+
+//TODO: change the loop back to numPages!  making it smaller for purposed of creating the website
+//TODO: get the background info and the video content
+/**
+ *  Uses the now playing API to get recent movie data
+ * @returns an object literal containing three arrays of recent movie data.
+ *          Includes all the titles (array), movie poster url (array), and rating (array) data
+ *          Info at elt `i` in any of these arrays corresponds to the same movie.
+ */
+async function getData(url) {
+    const newTitles = [];
+    const imgPathPiece = [];
+    const newRatings = [];
+    const newOverviews = [];
+
+
+    const response = await fetch(url);
+    const mainPage = await response.json();
+
+    const numPages = mainPage['total_pages'];
+    const numMovies = mainPage['total_results'];
+    // console.log('total pages:', numPages);
+    // console.log(' total results:', numMovies);
+
+    for (let i=1; i<= numPages; i++) {
+
+        const pageUrl = url + `&page=${i}`;
+
+        let response = await fetch(pageUrl);
+        let data = await response.json();
+        // console.log(data);
+        const movies = data['results'];
+
+
+        for (let movie of movies) {
+            // API has bug st some images don't render properly.  Don't include these in results for aesthetic purposes
+            if (movie['poster_path']) { 
+                newTitles.push(movie['title']);
+                imgPathPiece.push(movie['poster_path']);  
+                newRatings.push(movie['vote_average']);   
+                newOverviews.push(movie['overview']);
+            } 
+        }
+    }
+
+    console.log('length of non-null image results', newTitles.length);
+    // console.log('number of results matches:', titles.length === numMovies);
+    
+    return { newTitles, imgPathPiece, newRatings, newOverviews }; 
+
+}
+
+/**
+ * Uses the config API to get base url and potential poster sizes
+ * @returns a `baseURL` (string) and an array `posterSize` of different postersizes
+ */
+async function imageSetup(){
+    url = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+    // console.log('images', data['images']);
+    // console.log('baseurl', data['images']['base_url']);
+    // console.log('postersize', data['images']['poster_sizes']);
+
+    const baseUrl = data['images']['base_url'];
+    const possiblePosterSizes = data['images']['poster_sizes'];
+
+    return { baseUrl, possiblePosterSizes };
+}
+
+/**
+ * 
+ * @param {string} base the base URL from the confi api
+ * @param {string} possiblePosterSizes all possible poster sizes
+ * @param {string} specificPaths an array of strings representing the specific path to a movie poster
+ * @returns an array of strings, where each element concatenates base, size, and the specific path
+ */
+function genImgPath(base, possiblePosterSizes, specificPaths) {
+    const middleIx = Math.round(possiblePosterSizes.length/ 2);
+    const posterSize = possiblePosterSizes[middleIx - 1];
+
+    const fullPaths = [];
+    for (let specificPath of specificPaths) {
+        let newPath = base + posterSize + specificPath;
+        fullPaths.push(newPath);
+    }
+    return fullPaths;
+}
+
+function loadPage() {
+    const containerId = 'movies-grid';
+    displayPage(numLoads, imgPaths, titles, ratings, overviews, containerId);
+
+    numLoads += 1;
+    console.log('current normal mode page number', numLoads-1);
+
+    if (numLoads * moviesPerPage >= titles.length) { //If there are no more elements to show past this page hide "load more" button
+        console.log('trying to hide the movies button', 'titles length', titles.length);
+        load-more-movies-btn.classList.add('disabled'); //choosing hidden to not shift the movie imgs up
+    }
+}
+
+function loadSearchPage() {
+    console.log('within loadsearchpage');
+    const containerId = 'allSearchMovies';
+    console.log('search titles length:', searchTitles.length);
+    displayPage(numSearchLoads, searchImgPaths, searchTitles, searchRatings, searchOverviews, containerId);
+
+    numSearchLoads += 1;
+    console.log('current search page number:', numSearchLoads);
+
+        
+    if (numSearchLoads * moviesPerPage >= searchTitles.length) { //If there are no more elements to show past this page hide "load more" button
+        console.log('trying to hide the search button', 'searchtitles length', searchTitles.length);
+        // searchButton.style.visibility = 'hidden';  //choosing hidden to not shift the movie imgs up
+        searchButton.classList.add('disabled'); //choosing hidden to not shift the movie imgs up
+
+    }
+}
+
+
+/**
+ * Given the page number and under the assumption that each page contains 24 movies, appends the 24 movies information to the website
+ */
+function displayPage(pageIx, imgArr, titlesArr, ratingsArr, overviewsArr, mainContainer) {
+    // TODO: assert(imgPaths.length === titles.length === ratings.length);
+    console.log('\n');
+    console.log('pageIx:', pageIx);
+    const startingIx = pageIx*moviesPerPage;
+    const endingIx = (pageIx+1)*moviesPerPage;
+    const pageImgPaths = imgArr.slice(startingIx, Math.min(imgArr.length, endingIx));
+    const pageTitles = titlesArr.slice(startingIx, Math.min(titlesArr.length, endingIx));
+    const pageRatings = ratingsArr.slice(startingIx, Math.min(ratingsArr.length, endingIx));
+    const pageOverViews = overviewsArr.slice(startingIx, Math.min(overviewsArr.length, endingIx));
+
+    console.log('starting and ending index:', startingIx, endingIx);
+    let zip = (a, b, c, d) => a.map((elt,i) => [elt, b[i], c[i], d[i]]);
+
+    for (let [imgPath, title, rating, overview] of zip(pageImgPaths, pageTitles, pageRatings, overviews)) {
+        const singleMovieDiv = document.createElement('div');
+        singleMovieDiv.className = 'movie-card';
+
+        // create wrapper to contain the image and the overview
+        const imgWrapper = document.createElement('div');
+        imgWrapper.className = 'imgWrapper';
+
+        const img = document.createElement("img");
+        img.src = imgPath;
+        img.alt = 'movie poster';
+        img.classList.add('movie-poster', 'blur');
+        imgWrapper.appendChild(img);
+
+        const overviewTag = document.createElement("h5")
+        const overviewText = document.createTextNode(overview);
+        overviewTag.appendChild(overviewText);
+        overviewTag.classList.add('overview', 'fade');
+        imgWrapper.appendChild(overviewTag);
+
+
+        // create wrapper to contain the title and the rating
+        const infoWrapper = document.createElement('div');
+        infoWrapper.className = 'movieInfo';
+
+        //add title to infoWrapper
+        const titleTag = document.createElement("h5");
+        const titleText = document.createTextNode(title);
+        titleTag.appendChild(titleText);
+        titleTag.className = 'movie-title';
+        infoWrapper.appendChild(titleTag);
+
+        // add rating to infoWrapper
+        const ratingTag = document.createElement("h5")
+        const ratingText = document.createTextNode('⭐ ' + rating);
+        ratingTag.appendChild(ratingText);
+        ratingTag.className = 'movie-votes';
+        infoWrapper.appendChild(ratingTag);
+
+        const div = document.getElementById(mainContainer);
+
+        singleMovieDiv.appendChild(imgWrapper); 
+        singleMovieDiv.appendChild(infoWrapper);// add title + rating to DOM
+        div.appendChild(singleMovieDiv);
+
+    }
+
+}
+
+/**
+ * Make an API call using the searched term and display the new movies that match
+//  * @param {*} event search event
+ */
+async function search() {
+    console.log('\n');
+    clearSearchInfo();
+    document.getElementById("movies-grid").style.display = "none";
+    document.getElementById("movieButtonDiv").style.display = "none";
+
+    document.getElementById("allSearchMovies").style.display = "grid";
+    document.getElementById("searchButtonDiv").style.display = "grid";
+
+    submitted = true;
+    event.preventDefault();
+    const searchedVal = searchBarElt.value;
+    let { newTitles, imgPathPiece, newRatings, newOverviews } = await getSearchData(searchedVal);
+    searchTitles = newTitles;
+    searchRatings = newRatings;
+    searchOverviews = newOverviews;
+
+
+    let {baseUrl, possiblePosterSizes } = await imageSetup();
+
+    
+    const fullPaths = genImgPath(baseUrl, possiblePosterSizes, imgPathPiece);
+    searchImgPaths = fullPaths;
+    searchedSmth = false;
+    numSearchLoads = 0;
+    loadSearchPage();
+}
+
+function clearSearchInfo() {
+    document.getElementById("allSearchMovies").innerHTML= '';
+    searchTitles = [];
+    searchImgPaths = [];
+    searchRatings = [];
+    searchOverviews = [];
+    numSearchLoads = 0;
+    searchedSmth = false;
+
+    console.log('search info cleared!');
+
+}
+
+function closeSearch() {
+    console.log('close search hit!')
+    clearSearchInfo();
+    document.getElementById("allSearchMovies").style.display = "none";
+    document.getElementById("searchButtonDiv").style.display = "none";
+
+    document.getElementById("movies-grid").style.display = "grid";
+    document.getElementById("movieButtonDiv").style.display = "grid";
+    document.getElementById('search-input').value = '';
+}
+
+function scrollButtonDisplay() {
+    console.log('button clicked;'); 
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = "grid";
+      } else {
+        scrollButton.style.display = "none";
+      }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function scrollToTop() {
+    // document.body.scrollTop = 0;
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+
+}
+
+function forClearButton() {
+    console.log('for clear button');
+    closeSearch();
+    console.log('after close search clear button');
+
+}
+
+async function main() {
+    let { newTitles, imgPathPiece, newRatings, newOverviews } = await getMovieData();
+    titles = newTitles;
+    ratings = newRatings;
+    overviews = newOverviews;
+
+    let {baseUrl, possiblePosterSizes } = await imageSetup();
+    
+    const fullPaths = genImgPath(baseUrl, possiblePosterSizes, imgPathPiece);
+    imgPaths = fullPaths;
+    loadPage();
+}
+
+window.onload = () => {
+    document.getElementById("allSearchMovies").style.display = "none";
+    document.getElementById('searchButtonDiv').style.display = 'none';
+    main();
+}
+
+const searchBarElt = document.getElementById('search-input');
+
+const movieButton = document.getElementById('load-more-movies-btn');
+const searchButton = document.getElementById('searchButton');
+const scrollButton = document.getElementById('scrollButton');
+const clearButton  = document.getElementById('clearButton');
+
+document.addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        search();
+    }
+});
+
+
+searchBarElt.addEventListener('input', (event) => {
+    const value = searchBarElt.value;
+    console.log('form elt value:', value);
+    if (value) {
+        textEntered = true;
+    }
+
+    if (textEntered && submitted) {
+        console.log('something has been searched and has already been submitted');
+        console.log('current text:', value);
+
+        if(!value) {
+            console.log('there is no value left!  about to hit closesearch');
+            closeSearch();
+            console.log('after close search no more input text');
+        }
+    }
+});
+
+scrollButton.addEventListener('click', scrollToTop);
+
+
+
+
+
+
+
+
